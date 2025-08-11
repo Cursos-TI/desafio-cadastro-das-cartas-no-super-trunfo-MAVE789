a0f4a4138fa290cc316fe341e0c087fec0c065c0
MAVE789 <mhavezani@gmail.com>
2025-08-11 03:13:30 +0000

    DESAFIO NIVEL MESTRE

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -4,10 +4,12 @@ int main(){
     char estado1, estado2;
     char cod1[3], cod2[3];
     char cidade1[20], cidade2[20];
-    int pop1, pop2, pontot1, pontot2;
+    int  pontot1, pontot2;
     float area1, area2, pib1, pib2;
     float percapita1, percapita2;
     float densidade1, densidade2;
+    float superpoderA, superpoderB;
+    unsigned long int pop1, pop2;
 
     //CARTA 1
     printf("digite o estado da carta 1 estado: ");
@@ -36,7 +38,7 @@ int main(){
     scanf("%s", cidade2);
     printf("digite a população da carta 2: ");
     scanf("%d", &pop2);
-    printf("Qual a Area da carta 1: ");
+    printf("Qual a Area da carta 2: ");
     scanf("%f", &area2);
     printf("Qual o pib da cidade em bilhões de R$: ");
     scanf("%f", &pib2);
@@ -53,10 +55,10 @@ int main(){
     printf("Nome da cidade: %s\n",cidade1);
     printf("População: %d\n",pop1);
     printf("Area: %.2f Km2 \n",area1);
-    printf("PIB : %.1f Reais \n",pib1);
+    printf("PIB : %.2f Bilhões de Reais \n",pib1);
     printf("Pontos turisticos: %d \n",pontot1);
     printf("Densidade populacional: %.2f \n",densidade1);
-    printf("PIB per capita: %.2f \n",percapita1);
+    printf("PIB per capita: R$%.2f \n",percapita1);
 
     //APRESENTAÇÃO DA CARTA 2
     printf("\n\nCARTA 2:\n");
@@ -65,10 +67,24 @@ int main(){
     printf("Nome da cidade: %s\n",cidade2);
     printf("População: %d\n",pop2);
     printf("Area: %.2f Km2 \n",area2);
-    printf("PIB : %.1f Reais \n",pib2);
+    printf("PIB : %.2f Bilhões de Reais \n",pib2);
     printf("Pontos turisticos: %d \n",pontot2);
     printf("Densidade populacional: %.2f \n",densidade2);
-    printf("PIB per capita: %.2f \n",percapita2);
+    printf("PIB per capita: R$%.2f \n\n\n",percapita2);
+
+    //DUELO DAS CARTAS
+    superpoderA = (float) pop1 + area1 + pib1 + pontot1 + percapita1 + ( 1 / densidade1);
+    superpoderB =(float) pop2 + area2 + pib2 + pontot2 + percapita2 + ( 1 / densidade2);
+
+    printf("SE O RESULTADO FOR '1' A CARTA 1 VENCEU SE FOR '0' A CARTA 2 VENCEU.\n\n");
+
+    printf("População: %d\n", pop1 > pop2);
+    printf("Area: %d\n", area1 > area2);
+    printf("PIB: %d\n", pib1 > pib2);
+    printf("Pontos Turisticos: %d\n", pontot1 > pontot2);
+    printf("Densidade Populacional: %d\n", densidade1 > densidade2);
+    printf("PIB per capita: %d\n", percapita1 > percapita2);
+    printf("Super Poder: %d\n", superpoderA < superpoderB);
 
      return 0;
 }
